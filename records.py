150dfb6ae73da0058ae77420a0063fe570df3063
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-03-09 14:55:53 +0000

    cambiar doi a una publicacion

--- records.py
+++ records.py
@@ -1,6 +1,6 @@
 import requests
 headers = {"Content-Type": "application/json"}
-response = requests.get("https://zenodo.org/api/records/7700410",  headers=headers)
+response = requests.get("https://zenodo.org/api/records/7712947",  headers=headers)
 
 #responseList = requests.get('https://zenodo.org/api/records',
  #                       params={"q":"geosapi: GeoServer REST API R Interface"})
@@ -9,5 +9,5 @@ response = requests.get("https://zenodo.org/api/records/7700410",  headers=heade
 #los ids son los numeros del doi
 
 
-print(response.json()["metadata"]["title"])
+print(response.json())
 #print(responseList.json())
