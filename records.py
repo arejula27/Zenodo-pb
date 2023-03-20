557bfdc5385c34045f4c845b0a9021808f3c366d
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-03-20 10:47:38 +0000

    publications

--- records.py
+++ records.py
@@ -1,13 +1,14 @@
 import requests
 headers = {"Content-Type": "application/json"}
-response = requests.get("https://zenodo.org/api/records/7712947",  headers=headers)
+#response = requests.get("https://zenodo.org/api/records/7712947",  headers=headers)
 
-#responseList = requests.get('https://zenodo.org/api/records',
- #                       params={"q":"geosapi: GeoServer REST API R Interface"})
+responseList = requests.get('https://zenodo.org/api/records',
+                       params={'type': 'publication'})
 
 
 #los ids son los numeros del doi
 
+print(responseList.json()["hits"]["total"])
+#print(len(responseList.json()))
 
-print(response.json())
 #print(responseList.json())
