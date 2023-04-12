3b7be9e87f9554a3fd7391a40c53a7872db9bdac
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-04-12 08:34:10 +0000

    try catch

--- records.py
+++ records.py
@@ -2,13 +2,16 @@ import requests
 headers = {"Content-Type": "application/json"}
 #response = requests.get("https://zenodo.org/api/records/7712947",  headers=headers)
 
-responseList = requests.get('https://zenodo.org/api/records',
-                       params={'type': 'publication'})
+responseList = requests.get('https://zenodo.org/api/records', params={'type': 'publication', 'size': 5000, 'page': 4}) 
 
 
 #los ids son los numeros del doi
 
-print(responseList.json()["hits"]["total"])
+try:
+    print(len(responseList.json()["hits"]["hits"]))
+except:
+    print(responseList.json())
+
 #print(len(responseList.json()))
 
 #print(responseList.json())
