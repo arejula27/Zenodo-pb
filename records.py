cf35a6487f810439be2936930359a1f6f277d579
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-04-12 12:11:04 +0000

    peticiones semanalas

--- records.py
+++ records.py
@@ -1,17 +1,57 @@
+import datetime
+import json
 import requests
-headers = {"Content-Type": "application/json"}
-#response = requests.get("https://zenodo.org/api/records/7712947",  headers=headers)
+import time
 
-responseList = requests.get('https://zenodo.org/api/records', params={'type': 'publication', 'size': 5000, 'page': 4}) 
+HOUR_LIMIT_JSON = json.loads("{\"message\": \"2000 per 1 hour\", \"status\": \"429\"}")
+MIN_LIMIT_JSON = json.loads("{\"message\": \"60 per 1 minute\", \"status\": \"429\"}")
+ZENODO_SIZE_LIMIT=10000
 
 
-#los ids son los numeros del doi
+publications = []
+date = datetime.date(2009, 12, 31)
+week_delta = datetime.timedelta(days=7)
+first_rqt_time= datetime.datetime.now().time()
 
-try:
-    print(len(responseList.json()["hits"]["hits"]))
-except:
-    print(responseList.json())
+while date < datetime.date.today():
 
-#print(len(responseList.json()))
+    start_date = date
+    end_date = date + week_delta
 
-#print(responseList.json())
+    responseList = requests.get('https://zenodo.org/api/records', params={'type': 'publication', 'q': f'publication_date:[{start_date} TO {end_date}]', 'size': ZENODO_SIZE_LIMIT})
+
+    try:
+        print(f"Trying {start_date} - {end_date}.")
+        if len(responseList.json()['hits']['hits']) < ZENODO_SIZE_LIMIT:
+            print(f"{len(responseList.json()['hits']['hits'])} publications found.")  
+            publications.extend(responseList.json()['hits']['hits'])
+    
+        else:
+            print(f"Week {start_date} - {end_date} have more than {ZENODO_SIZE_LIMIT} records.Performing daily queries...")
+            day_delta = datetime.timedelta(days=1)
+            current_date = start_date
+            while current_date <= end_date:
+                print(f"Trying {current_date}.")
+                responseList = requests.get('https://zenodo.org/api/records', params={'type': 'publication', 'q': f'publication_date:{current_date}', 'size': ZENODO_SIZE_LIMIT})
+                print(f"{len(responseList.json()['hits']['hits'])} publications found.") 
+                if len(responseList.json()['hits']['hits']) >= ZENODO_SIZE_LIMIT:
+                    print(f"Day {current_date} have more than {ZENODO_SIZE_LIMIT} records. Data will be lost")
+                publications.extend(responseList.json()['hits']['hits'])
+                current_date += day_delta
+        date += week_delta+datetime.timedelta(days=1)
+    except:
+        if responseList.json()==MIN_LIMIT_JSON:
+            time.sleep(50)
+        elif responseList.json()==HOUR_LIMIT_JSON:
+             remaining_time = datetime.timedelta(hours=1)-first_rqt_time
+             print( f"Request limit per hour reached, the process will sleep{remaining_time}")
+             time.sleep(remaining_time)
+             first_rqt_time= datetime.datetime.now().time()
+
+
+   
+
+  
+   
+
+print(f"Se encontraron {len(publications)} publicaciones.")    
